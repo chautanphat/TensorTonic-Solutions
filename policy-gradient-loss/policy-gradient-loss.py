93450c7a4b84d2aa5b1bf74fac9626f8de9a62c2
Chau Tan Phat <ctanphat07@gmail.com>
2026-04-25 23:30:26 +0700

    Update solution for policy-gradient-loss

--- policy-gradient-loss/policy-gradient-loss.py
+++ policy-gradient-loss/policy-gradient-loss.py
@@ -7,7 +7,7 @@ def policy_gradient_loss(log_probs, rewards, gamma):
     T = len(log_probs)
     G = np.zeros(T)
     G[T - 1] = rewards[T - 1]
-    for t in range (T-2, -1, -1): G[t] = rewards[t] + gamma * G[t + 1]
+    for t in range (T- 2 , -1, -1): G[t] = rewards[t] + gamma * G[t + 1]
     mean_G = np.mean(G)
     A = G - mean_G
     L = -np.mean(log_probs * A)
